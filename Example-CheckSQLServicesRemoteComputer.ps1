3cb7c3d36a253e41f2477c66bb40865da26946c2
Merle <57601115+mcarr10@users.noreply.github.com>
2022-06-11 11:04:58 -0400

    Added comments

--- Example-CheckSQLServicesRemoteComputer.ps1
+++ Example-CheckSQLServicesRemoteComputer.ps1
@@ -1,5 +1,5 @@
-#
-#
+# Powershell script to that returns services named like SQL
+# 2022-06-11
 
 $ComputerName = read-host -prompt "Enter computer name"
 get-service -ComputerName $ComputerName | Where-object {$_.name -like '*sql*'}
